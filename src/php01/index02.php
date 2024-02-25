f693efcfe20099cde44f28d1e3e9ddc2cef2e322
ryosuke miyoshi <m.ryosuke1003@gmail.com>
2024-02-25 16:27:33 +0900

    サンプルファイルの追加

---
+++ src/php01/index02.php
@@ -0,0 +1,8 @@
+<?php
+$name = "Tanaka";
+$last_name = "Yamada";
+$first_name = "Saburo";
+
+echo $name . "Jiro";
+echo "<br />";
+echo $first_name . $last_name;
---
+++ src/php01/index06.php
@@ -0,0 +1,18 @@
+<?php
+
+function getSquareArea($base, $height)
+{
+    return $base * $height;
+}
+function getTriangleArea($base, $height)
+{
+    return $base * $height / 2;
+}
+function getTrapeZoidArea($upperBase, $lowerBase, $height)
+{
+    return ($upperBase + $lowerBase) * $height / 2;
+}
+
+echo getSquareArea(5, 5) . "\n";
+echo getTriangleArea(7, 8) . "\n";
+echo getTrapeZoidArea(4, 5, 4);
